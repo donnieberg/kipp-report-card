4088e4e170aac527061483cdbeff96053ee9526a
Donielle Berg <donielle.berg@gmail.com>
2013-08-18 22:57:26 -0700

    sessions part I

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :school_id, :type, :password, :password_confirmation
 
-  before_save { self.email = email.downcase }
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -12,5 +14,20 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   has_many :ratings, foreign_key: "rater_id"
+
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
 end
