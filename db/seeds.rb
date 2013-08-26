4760b2dd9ff947e0e69b3564f092ad2c98efcc0b
Donielle Berg <donielle.berg@gmail.com>
2013-08-25 23:43:48 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+School.delete_all
+User.delete_all
+Course.delete_all
+Category.delete_all
+Char_strength.delete_all
+
+
+#schools
+School.create(name: "KIPP SF Bay", city:"San Francisco", state:"CA", grade_level:"Middle")
+School.create(name: "KIPP Heartwood", city:"San Francisco", state:"CA", grade_level:"High")
 
 #teachers
 User.create(name: "Avand", school_id: 1, type: "Teacher", password:"foobar", password_confirmation: "foobar")
+User.create(name: "Jackie", school_id: 1, type: "Teacher", password:"foobar", password_confirmation: "foobar")
+User.create(name: "Anil", school_id: 2, type: "Teacher", password:"foobar", password_confirmation: "foobar")
+User.create(name: "Markus", school_id: 2, type: "Teacher", password:"foobar", password_confirmation: "foobar")
+
+#courses
+Course.create(title:"Rails 4", teacher_id:1)
+
+#students
+User.create(name: "Sharif", school_id: 1, type: "Student", password:"foobar", password_confirmation: "foobar", grade_level: 7)
+User.create(name: "Ian", school_id: 1, type: "Student", password:"foobar", password_confirmation: "foobar", grade_level: 7)
+User.create(name: "Alex", school_id: 2, type: "Student", password:"foobar", password_confirmation: "foobar", grade_level: 10)
+User.create(name: "Donielle", school_id: 2, type: "Student", password:"foobar", password_confirmation: "foobar", grade_level: 10)
+
+
+
+
