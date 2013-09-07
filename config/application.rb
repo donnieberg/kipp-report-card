1dbab41c4ee6a18b7a88ee03f070a8dccc88d13b
Donielle Berg <donielle.berg@gmail.com>
2013-09-07 15:08:04 -0700

    edit application.rb to push to heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module KippReportCard
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    #to fix heroku errors
+    config.assets.initialize_on_precompile = false
   end
 end
