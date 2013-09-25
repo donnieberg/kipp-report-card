3a65cb5efc40927917c6f6a5d38a35c1aabfce1a
Donielle Berg <donielle.berg@gmail.com>
2013-09-24 22:47:48 -0700

    refactored javascript

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,10 +2,6 @@ $(function () {
   var average = "Average";
   var rater = $('.rater-name').html();
   var rater2 = $('.student-last-name').html();
-  // $(".raw-data tr:first td:last").after("<td>" + average + "</td>");
-  // $(".raw-data tr:first td:last").after("<td>" + rater + "</td>");
-  // $(".raw-data tr:first td:last").after("<td>" + rater2 + "</td>");
-
 
   //Responsive Tables
   $('.footable').footable();
@@ -16,6 +12,27 @@ $(function () {
   $('.carousel').carousel({
     interval: 4000
   });
+
+  $('.category-name').each(function(){
+    if($(this).html() == "Zest") {
+      $(this).prepend("<i class='icon-thumbs-up-alt icon-large category-icon'></i>");
+    } else if ($(this).html() == "Grit") {
+      $(this).prepend("<i class='icon-cloud-upload icon-large category-icon'></i>");
+    } else if ($(this).html() == "School Work") {
+      $(this).prepend("<i class='icon-file-text-alt icon-large category-icon'></i>");
+    } else if ($(this).html() == "Interpersonal") {
+      $(this).prepend("<i class='icon-group icon-large category-icon'></i>");
+    } else if ($(this).html() == "Optimism") {
+      $(this).prepend("<i class='icon-smile icon-large category-icon'></i>");
+    } else if ($(this).html() == "Gratitude") {
+      $(this).prepend("<i class='icon-heart-empty icon-large category-icon'></i>");
+    } else if ($(this).html() == "Social Intelligence") {
+      $(this).prepend("<i class='icon-comments icon-large category-icon'></i>");
+    } else if ($(this).html() == "Curiosity") {
+      $(this).prepend("<i class='icon-eye-open icon-large category-icon'></i>");
+    }
+  });
+
 
 });
 
