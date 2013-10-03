e631846ead9780c8abca5e0412299b3b501f3f8c
donnieberg <donielle.berg@gmail.com>
2013-10-03 16:35:29 -0700

    add student id to migration file

--- db/migrate/20130926020431_create_report_cards.rb
+++ db/migrate/20130926020431_create_report_cards.rb
@@ -1,6 +1,7 @@
 class CreateReportCards < ActiveRecord::Migration
   def change
     create_table :report_cards do |t|
+      t.integer :student_id
       t.integer :quarter
       t.integer :year
 
